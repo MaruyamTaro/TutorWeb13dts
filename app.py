cf369d77982cc66ea3ec960af610a75b59cababc
MaruyamTaro <21300@wc.school.nz>
2025-03-10 09:36:21 +1300

    INITAL

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_homepage():
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def render_menu():
+    return render_template('menu.html')
+
+
 
 
 if __name__ == '__main__':
     app.run()
+
