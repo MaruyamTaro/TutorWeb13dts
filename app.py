da55767569b7e91cf83753a8404512a727e79de8
school comp <21300@wc.school.nz>
2025-03-12 14:20:40 +1300

    Added some DB

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
 
 app = Flask(__name__)
+def connect_database(db_file):
+    """
+    Creates a connection with the database
+    :param db_file:
+    :return: conn
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+        print(f'an error when connecting the db')
+    return
 
 
 @app.route('/')
@@ -10,7 +26,7 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu():
-    return render_template('menu.html')
+    return render_template('Schedule.html')
 
 
 
