38761c26199009d8fb7bacb116d3691b535b38b9
MaruyamTaro <21300@wc.school.nz>
2025-03-16 17:49:21 +1300

    gave up on figuring out insert so went to just display the table. was not able to figure out why its not working. sqlite3.OperationalError: no such table: TimeTable. when it is there

--- app.py
+++ app.py
@@ -24,11 +24,25 @@ def render_homepage():
     return render_template('home.html')
 
 
-@app.route('/menu')
-def render_menu():
-    return render_template('Schedule.html')
+@app.route('/Schedule')
+def render_schedule():
+    con = connect_database(DATABASE)
+    query = ("SELECT Teacher_f_name, Student_f_name, Time FROM TimeTable")
+    cur = con.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    print(results)
+    con.close()
+    return render_template('menu.html', sessions=results)
+
+
+
+
+
+
 
 @app.route('/signup', methods=['POST','GET'])
+
 def render_signup():
     if request.method == 'POST':
         fname = request.form.get('user_F_name').title().strip()
@@ -46,14 +60,10 @@ def render_signup():
             return redirect("\signup?error=password+must+be+more+than+8+letters")
 
         con = connect_database(DATABASE)
-        query_insert = "INSERT INTO People (fname, lname, email, pass1, teachquestion) VALUES (?, ?, ?, ?)"
+        query_insert = ("INSERT INTO People (First_name, Last_name, Email, password, Teacher) "
+                        "VALUES (fname, lname, email, pass1, teachquestion)")
         cur = con.cursor()
         cur.execute(query_insert, (fname, lname, email, pass1, teachquestion))
-
-
-
-    return render_template('signup.html')
-
 @app.route('/login', methods=['POST','GET'])
 def render_login():
     return render_template('login.html')
@@ -64,3 +74,4 @@ def render_login():
 if __name__ == '__main__':
     app.run()
 
+print("WHYYYYY")
